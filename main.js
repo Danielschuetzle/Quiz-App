c0c7493f0a1965670f05398636af47c2ff46bf5a
Daniel Schützle <danielschuetzle@gmail.com>
2023-05-05 10:45:21 +0200

    form page added, bookmark icon toogle added

--- main.js
+++ main.js
@@ -7,3 +7,20 @@ document.addEventListener("DOMContentLoaded", () => {
     })
   );
 });
+document.addEventListener("DOMContentLoaded", () => {
+  // Toggle bookmark icon
+  document.querySelectorAll(".icon").forEach((icon) =>
+    icon.addEventListener("click", () => {
+      icon.classList.toggle("bookmarked");
+    })
+  );
+
+  // Toggle answer visibility and update button text
+  document.querySelectorAll(".answer-button").forEach((button) =>
+    button.addEventListener("click", () => {
+      const answerParagraph = button.nextElementSibling;
+      const isHidden = answerParagraph.classList.toggle("hidden");
+      button.textContent = isHidden ? "Antwort anzeigen" : "Antwort verbergen";
+    })
+  );
+});
